d49a1abca55555de229e942336ff8c26d10dd0b6
Dumay <104175742+CoffeeCapy@users.noreply.github.com>
2026-02-21 04:25:55 +0700

    hunter.py

--- hunter.py
+++ hunter.py
@@ -1,32 +1,27 @@
 import secrets
 import base58
-from solana.keypair import Keypair
+from solders.keypair import Keypair # Ini perubahan utamanya
 from solana.rpc.api import Client
 
-# Menggunakan koneksi resmi Solana
 client = Client("https://api.mainnet-beta.solana.com")
 
 def hunt():
-    print("--- Memulai Pencarian CoffeeCapy ---")
-    # Cek 100 dompet setiap kali mesin ini menyala
+    print("--- Hunting Started ---")
     for i in range(100):
-        seed = secrets.token_bytes(32)
-        keypair = Keypair.from_seed(seed)
-        address = str(keypair.public_key)
-        priv = base58.b58encode(seed).decode('ascii')
+        # Membuat keypair baru
+        kp = Keypair() 
+        address = str(kp.pubkey())
+        # Private key dalam format Base58
+        priv = base58.b58encode(bytes(kp)).decode('utf-8')
         
         try:
-            # Cek saldo langsung ke blockchain
-            res = client.get_balance(keypair.public_key)
-            bal = res['result']['value'] / 1000000000
+            res = client.get_balance(kp.pubkey())
+            bal = res.value / 1000000000 # Format terbaru pake .value
+            print(f"[{i+1}] {address} | Bal: {bal} SOL")
             
-            print(f"[{i+1}] {address} | Saldo: {bal} SOL")
-            
-            # Jika ditemukan dompet yang ada isinya:
             if bal > 0:
-                print(f"!!! JACKPOT DITEMUKAN !!!")
                 with open("jackpot.txt", "a") as f:
-                    f.write(f"Address: {address} | PrivateKey: {priv} | Balance: {bal}\n")
+                    f.write(f"Addr: {address} | Priv: {priv} | Bal: {bal}\n")
         except:
             continue
 
